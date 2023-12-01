051484bb68b07a2d096ead5416044e663a38f5ca
Eliana-2 <eliana.guizar1@gmail.com>
2023-11-30 18:40:41 -0800

    Add logic to create server and serve html files

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer((req, res) => {
+  const reqFilePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : 
+    path.extname(req.url) !== '' ? req.url : `${req.url}.html`);
+
+  console.log(reqFilePath);
+  fs.readFile(reqFilePath, (err, data) => {
+    if (err) {
+      if(err.code === 'ENOENT') {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, data) => {
+          res.writeHead(200, {'Content-Type' : 'text/html'});
+          res.end(data);
+        })
+      } 
+      else {
+        res.writeHead(500);
+        res.end('Server Error');
+      }
+      console.log(err.code === 'ENOENT');
+    }
+    else {
+      let contentType = 'text/html';
+      switch(path.extname(reqFilePath)) {
+        case '.ico':
+          contentType = 'image/x-icon';
+          break;
+      }
+
+      res.writeHead(200, {'Content-Type': contentType});
+      res.end(data);
+    }
+  })
+}).listen(8080, () => console.log('Server running'));
